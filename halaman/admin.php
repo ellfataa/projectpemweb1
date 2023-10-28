5f354433f407043eb932ae72a5668cfa8e69d505
DanielAArif <147832572+DanielAArif@users.noreply.github.com>
2023-10-28 23:24:30 +0700

    Membuat halaman admin setelah melakukan log in

--- halaman/admin.php
+++ halaman/admin.php
@@ -16,10 +16,45 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="admin.css" type="text/css">
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=PT+Sans:wght@400;700&family=Poppins:wght@300;400;500;600;700&display=swap" rel="stylesheet">
     <title>Admin</title>
 </head>
 <body>
-    <center>
+    <nav>
+        <div class="toggle">
+            <input type="checkbox" />
+            <span></span>
+            <span></span>
+            <span></span>
+        </div>
+        <ul>
+            <li><a href="">Dashboard</a></li>
+            <li><a href="">Kelas</a></li>
+            <li><a href="">Kalender</a></li>
+            <li><a href="">Setelan</a></li>
+        </ul>
+        <div class="logo">
+            <h4>Sekul Legend</h4>
+        </div>
+        <div class="logo_profil">
+            <a href="profil.php"><img src="profil.jpg" alt="profil"/></a>
+        </div>
+    </nav>
+    <div class="row1">
+        <div>
+            <img src="../gambar/logo.svg" width="200px" style="margin: 50px;">
+        </div>
+        <div class="row1_2">
+            <h2>Administrator</h2>
+            <p>Selamat Datang, <?php echo $_SESSION['user']; ?></p>
+        </div>
+    </div>
+
+
+    <!-- <center>
         <div id="app">
             <nav>
                 <ul>
@@ -37,7 +72,14 @@
         <br>
         <h1>Selamat datang, <?php echo $_SESSION['user']; ?></h1>
         Halaman setelah login
-    </center>
-    
+    </center> -->
+    <script>
+        const toggle = document.querySelector('.toggle input');
+        const nav = document.querySelector('nav ul');
+
+        toggle.addEventListener('click', function() {
+            nav.classList.toggle('slide');
+        });
+    </script>
 </body>
 </html>
